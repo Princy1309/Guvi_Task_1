7720f4bb8cbf25657a10c24fbb37317d2687a267
Princy1309 <princym1309@gmail.com>
2025-07-26 18:25:52 +0530

    Completed all task

---
+++ Solution3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Solution4 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner (System.in);
+		
+		int input =obj.nextInt();
+		
+		if(input<0) {
+			System.out.println("Negative");
+		}
+		else {
+			System.out.println("Positive");
+		}
+		obj.close();
+    }
+}
